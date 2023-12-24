4ec9e83fb71d85c0ef300456944fb4cd4b828984
SONOLET Aymeric <aymeric.sonolet@cea.fr>
2023-12-24 14:59:31 +0100

    Version 0.1

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from setuptools import setup
 
 setup(
     name='pre_commit_placeholder_package',
-    version='0.0.0',
+    version='0.1.0',
     install_requires=['clang-format==17.0.6', 'clang-tidy==14.0.6'],
 )
